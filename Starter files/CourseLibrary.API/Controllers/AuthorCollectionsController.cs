c3f036d34e85b4e54c75dc747bb9ab74a5e24170
Ahmed Attia <El2_attia@outlook.com>
2023-06-07 13:20:29 +0300

    create a collection of authors

---
+++ Starter files/CourseLibrary.API/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Entities;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseLibrary.API.Controllers
+{
+    [Route("api/authorcollections")]
+    [ApiController]
+    public class AuthorCollectionsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorCollectionsController(
+            ICourseLibraryRepository courseLibraryRepository,
+            IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<AuthorDto>>> CreateAuthorCollection(IEnumerable<AuthorForCreationDto> authorCollection)
+        {
+            var authors = _mapper.Map<IEnumerable<Author>>(authorCollection);
+            foreach (var author in authors)
+            {
+                _courseLibraryRepository.AddAuthor(author);
+            }
+            await _courseLibraryRepository.SaveAsync();
+            return StatusCode(401, authors);
+        }
+    }
+}
