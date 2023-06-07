caa929418389ba77e3fbf0888a5bff0300752edd
Ahmed Attia <El2_attia@outlook.com>
2023-06-07 12:58:38 +0300

    Create child resources together with parent resource

--- Starter files/CourseLibrary.API/Models/AuthorForCreationDto.cs
+++ Starter files/CourseLibrary.API/Models/AuthorForCreationDto.cs
@@ -6,5 +6,7 @@
         public string LastName { get; set; } = string.Empty;
         public DateTimeOffset DateOfBirth { get; set; }
         public string MainCategory { get; set; } = string.Empty;
+        public ICollection<CourseForCreationDto> Courses { get; set; } = new List<CourseForCreationDto>();
+        //for creating course collection at the same time with author
     }
 }
